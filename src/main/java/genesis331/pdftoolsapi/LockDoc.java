87361089490a986530e3e0c553d50dbc9bee4c30
genesis331 <cheah3838@gmail.com>
2022-12-31 16:12:48 +0800

    add protect feature

--- src/main/java/genesis331/pdftoolsapi/LockDoc.java
+++ src/main/java/genesis331/pdftoolsapi/LockDoc.java
@@ -6,26 +6,35 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.encryption.AccessPermission;
+import org.apache.pdfbox.pdmodel.encryption.StandardProtectionPolicy;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 
 @WebServlet(name = "lock", value = "/lock")
 @MultipartConfig
 public class LockDoc extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "LockDoc";
-    }
+    public void init() {}
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        response.setContentType("application/json");
-        PrintWriter out = response.getWriter();
         if (request.getPart("file") != null) {
-            out.print("{\"message\": \"" + message + "\"}");
+            response.setContentType("application/pdf");
+            OutputStream out = response.getOutputStream();
+            PDDocument document = Loader.loadPDF(request.getPart("file").getInputStream());
+            StandardProtectionPolicy spp = new StandardProtectionPolicy("password", "password", new AccessPermission());
+            response.setHeader("Content-Disposition", "attachment; filename=\"locked-" + request.getPart("file").getSubmittedFileName() + ".pdf\"");
+            document.protect(spp);
+            document.save(out);
+            out.flush();
         } else {
-            out.print("{\"error\": \"No file provided.\"}");
+            response.setContentType("application/json");
+            PrintWriter out2 = response.getWriter();
+            out2.print("{\"error\": \"No file provided.\"}");
         }
     }
 
