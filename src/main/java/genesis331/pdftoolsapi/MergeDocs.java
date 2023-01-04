effa4dcb2187a7c675b6c53e422d7afe837636f2
Cheah Zixu <cheah3838@gmail.com>
2023-01-04 21:08:27 +0800

    add merge feature
    * updated merge docs API
    
    * remove template code
    
    Co-authored-by: Xytrix1103 <syho031001@gmail.com>

--- src/main/java/genesis331/pdftoolsapi/MergeDocs.java
+++ src/main/java/genesis331/pdftoolsapi/MergeDocs.java
@@ -1,25 +1,48 @@
 package genesis331.pdftoolsapi;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+import org.apache.pdfbox.multipdf.PDFMergerUtility;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.ByteArrayOutputStream;
 
 @WebServlet(name = "merge", value = "/merge")
+@MultipartConfig
 public class MergeDocs extends HttpServlet {
-    private String message;
+    
+    public void init() {}
 
-    public void init() {
-        message = "MergeDocs";
-    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        if(request.getParts().isEmpty() != true) {
+            PDFMergerUtility PDFmerger = new PDFMergerUtility();
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("application/json");
-        PrintWriter out = response.getWriter();
-        out.print("{\"message\": \"" + message + "\"}");
+            for (Part part : request.getParts()) {
+                PDFmerger.addSource(part.getInputStream());
+            }
+
+            PDFmerger.setDestinationStream(out);
+            PDFmerger.setDestinationFileName("merged.pdf");
+            PDFmerger.mergeDocuments(null);
+
+            response.setContentLength(out.size());
+            response.setContentType("application/pdf");
+            response.setHeader("Content-Disposition", "attachment; filename=merged.pdf");
+            response.getOutputStream().write(out.toByteArray());
+
+            out.flush();
+        } else {
+            response.setContentType("application/json");
+            PrintWriter out2 = response.getWriter();
+            out2.print("{\"error\": \"No file provided.\"}");
+        }
     }
 
     public void destroy() {}
