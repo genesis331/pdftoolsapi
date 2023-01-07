5cf6b1c23337ffc7a58a2701cbceb4141e0a6370
Xytrix1103 <syho031001@gmail.com>
2023-01-07 18:11:05 +0800

    updated organize docs API

--- src/main/java/genesis331/pdftoolsapi/OrganizeDoc.java
+++ src/main/java/genesis331/pdftoolsapi/OrganizeDoc.java
@@ -1,14 +1,23 @@
 package genesis331.pdftoolsapi;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPageTree;
+import org.apache.pdfbox.pdfwriter.compress.CompressParameters;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.io.PrintWriter;
+import java.io.OutputStream;
 
 @WebServlet(name = "organize", value = "/organize")
+@MultipartConfig
 public class OrganizeDoc extends HttpServlet {
     private String message;
 
@@ -16,10 +25,29 @@ public class OrganizeDoc extends HttpServlet {
         message = "OrganizeDoc";
     }
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("application/json");
-        PrintWriter out = response.getWriter();
-        out.print("{\"message\": \"" + message + "\"}");
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("application/pdf");
+        if (request.getPart("file") != null) {
+            String[] pages = request.getParameter("pages").split(",");
+            PDDocument document = Loader.loadPDF(request.getPart("file").getInputStream());
+            PDPageTree pagesTree = document.getPages();
+            PDDocument newDocument = new PDDocument();
+            OutputStream out = response.getOutputStream();
+            ArrayList<Integer> pagesList = new ArrayList<Integer>();
+
+            for (String page : pages) {
+                pagesList.add(Integer.parseInt(page));
+            }
+
+            for (int i = 0; i < pagesList.size(); i++) {
+                newDocument.addPage(pagesTree.get(pagesList.get(i)));
+            }
+            
+            response.setHeader("Content-Disposition", "attachment; filename=" + request.getPart("file").getSubmittedFileName() + ".pdf\"");
+            newDocument.save(out, CompressParameters.DEFAULT_COMPRESSION);
+            out.flush();
+
+        }
     }
 
     public void destroy() {}
